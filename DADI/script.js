4fbd3b2d484177136ad0eb30d82324824188865c
alessandropecchini99 <pecchini99@gmail.com>
2023-04-17 17:08:47 +0200

    add index / style / script

---
+++ DADI/script.js
@@ -0,0 +1,14 @@
+/* MAIL */
+// 1. Chiedere all'utente la sua mail
+// 2. Controllare nella lista chi può accedere
+// 3. Alert del risultato
+
+
+
+
+
+
+
+/* DADI */
+// 1. Generare un numero da 1 a 6 per utente e pc
+// 2. vince chi fa il punteggio più alto (o pareggio)
---
+++ MAIL/script.js
@@ -0,0 +1,29 @@
+/* MAIL */
+// 1. Chiedere all'utente la sua mail {prompt}
+// 2. Controllare nella lista chi può accedere {for}
+    /*  RIPETI finchè è VERO i < lista mail 
+            SE arrey[i] è presente, settare true
+            ALTRIMENTI lasciare false
+            poi mostrare il risultato
+        FINE CICLO */
+// 3. Alert del risultato {alert}
+
+
+
+
+// 1. DICHIARO LE VARIABILI
+// mail inserita
+const userMail = prompt('Inserisci la tua mail:');
+console.log(`Mail inserita:` + ` ` + userMail);
+// variabile booleana di controllo
+let ctrl = false;
+
+
+// 1. ARRAY - LISTA DELLE MAIL
+let mail = [`alessandro@boolean.it`, `pecchini@boolean.it`, `henri@boolean.it`, `francesco@boolean.it`, `giuseppe@boolean.it`];
+console.log(mail);
+
+// 2. CONTROLLO DELLA LISTA MAIL
+for (i = 0; i < mail.length; i++) {
+    if (mail[i] == userMail)
+}
